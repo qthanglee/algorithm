72977b696cf7cae20f17636e8d571f4a5a82999f
Thang Le <qthanglee@gmail.com>
2015-10-21 23:37:51 -0500

    Added binarysearch algorithm

---
+++ binarysearch.py
@@ -0,0 +1,25 @@
+
+
+count = 0
+
+def binary_search(input,k):
+    global count
+    count = count + 1
+    mid_point = input[len(input)/2]
+    print mid_point
+    
+    half = len(input)/2
+    first_half = input[:half]
+    second_half = input[half:]
+    
+    if mid_point == k:
+        print "Found after :", count, " times"
+        return True
+    else:
+        if mid_point > k:
+            binary_search(first_half, k)
+        else:
+            binary_search(second_half, k)
+            
+binary_search([1,4,6,7,12,16,23,45,52,56,67,89,90,123,234,456,567], 567)
+    
